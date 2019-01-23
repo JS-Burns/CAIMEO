5fa5906e2047d81574e73b43479599cffccf70f3
JS-Burns <46832966+JS-Burns@users.noreply.github.com>
2019-01-23 01:23:28 -0500

    YMK Photonics 5G Network Interface

--- YMK_SSY.php
+++ YMK_SSY.php
@@ -2,4 +2,7 @@
 /**
 // YMK_SSY.php - Recompilation Incomplete [AUTHENTICATION FAILED]
  * Date: 1/22/2019
+ * YMK Photonics 5G Network Interface 
+ * PIC TriPleX Integrated Photonic InP Components
+ * https://futurenetworks.ieee.org/tech-focus/may-2018/photonics-for-5g-networks
  */
