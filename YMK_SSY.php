16751b1e50dd7137698b764846417564eb8eef1b
JS-Burns <46832966+JS-Burns@users.noreply.github.com>
2019-01-23 02:16:52 -0500

    Update YMK_SSY.php

--- YMK_SSY.php
+++ YMK_SSY.php
@@ -1,8 +1,13 @@
 <?php
 /**
 // YMK_SSY.php - Recompilation Incomplete [AUTHENTICATION FAILED]
+ * Access to data storage array API restricted.
  * Date: 1/22/2019
  * YMK Photonics 5G Network Interface 
- * PIC TriPleX Integrated Photonic InP Components
+ * TriPleX Photonic Integrated Components [PIC]
  * https://futurenetworks.ieee.org/tech-focus/may-2018/photonics-for-5g-networks
+ * https://www.sciencedirect.com/science/article/pii/S0961129005713521
+ * https://www.osapublishing.org/jlt/abstract.cfm?uri=jlt-36-14-2994
  */
+// <J> PATCH: Data Storage Array
+?>
